f08172a4a281fc6c8e27967ec63f4e364ee1be07
sanket <sanketjain2018@gmail.com>
2026-07-04 12:48:15 +0530

    feat: initialize spring boot backend project

---
+++ backend/mirachi-backend/src/main/java/com/mirachi/MirachiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mirachi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MirachiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MirachiBackendApplication.class, args);
+	}
+
+}
